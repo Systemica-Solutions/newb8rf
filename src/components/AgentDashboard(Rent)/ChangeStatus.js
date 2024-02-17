af83a2d0769e58cd87e49248b4760ff66b15b6b2
GauravB8R <gauravbhalerao@systemicasolutions.com>
2024-02-17 19:58:24 +0530

    changed the header of delisting

--- src/components/AgentDashboard(Rent)/ChangeStatus.js
+++ src/components/AgentDashboard(Rent)/ChangeStatus.js
@@ -555,7 +555,7 @@ function ChangeStatus() {
           <div>
             <form className="login-form" onSubmit={submitRent}>
               <p className="text-[1.2rem] py-[1rem]">
-                <b>Write your Feedback here</b>
+              <b>Write the reason for Delisting</b>
               </p>
               <div className="px-[1rem]">
                 <div
@@ -585,7 +585,7 @@ function ChangeStatus() {
                     placeholder="
                     enter your feedback"
                   >
-                    Enter Feed Back
+                   Enter your reason
                   </textarea>
                 </div>
               </div>
